c8705962a99f1a0b20284a71518a0345ead712c5
Robert Ragans <robertragans@Roberts-MacBook-Pro-2.local>
2019-01-16 11:50:53 -0500

    first commit

---
+++ express101.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello, world</h1>")
+})
+
+app.get('/class',(req,res)=>{
+    res.send("<p>Hello, class</p>")
+})
+
+
+
+app.listen(24983)
